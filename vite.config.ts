74055188bdd16e547a576278a0b8a943aa768538
Daniil <shklyaevdd@gmail.com>
2025-12-27 16:02:24 +0300

    iterration 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/',
   server: {
     port: 8080,      // Теперь ссылка всегда будет http://localhost:3000
     host: true,      // Позволяет зайти на сайт с телефона через твой IP (например, 192.168.1.10:3000)
